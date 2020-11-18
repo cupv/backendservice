247e607cc2f9fd111e711a76d31f1967413d406f
Hoang Nguyen <hoangnguyen121098@gmail.com>
2020-11-18 11:15:55 +0700

    update model campaign, lesson

--- Models/Lession.cs
+++ Models/Lession.cs
@@ -9,6 +9,19 @@ namespace API.Models
     {
         public string Name { set; get; }
         public Guid CourseId { set; get; }
+        public string Content { get; set; }
         public Course Course { set; get; }
+    }
+
+    public class Content
+    {
+        public List<Unit> Units { get; set; }
+    }
+
+    public class Unit
+    {
+        public string Label { get; set; }
+
+        public string Url { get; set; }
     }
 }
